f77ea91189d9ce4db641d09342f2c4ee59736a5f
Administrador <Administrador@2DAM01>
2023-11-08 19:25:08 +0100

    Ha crecido

--- src/holaClase/HolaClase.java
+++ src/holaClase/HolaClase.java
@@ -5,6 +5,9 @@ public class HolaClase {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 System.out.println("Hola Clase");
+System.out.println("Adios Clase");
+System.out.println("y ahora a casa");
+
 	}
 
 }
